d5bf8213c5e42296ba5afa0dd3f333ddf639d9c7
kakposoe <info@designofka.com>
2017-02-15 17:07:03 +0000

    Update to gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,53 @@
 var paths = {
-  build: './build/min',
-	stylesheet: './assets/css'
+	build: './build/scss/main.scss',
+	stylesheet: './assets/css/'
 };
 
 var gulp = require('gulp'),
-    gulpLoadPlugins = require('gulp-load-plugins'),
-    plugins = gulpLoadPlugins(); // Last Injection
+	gulpLoadPlugins = require('gulp-load-plugins'),
+		browserSync = require('browser-sync').create(),
+	plugins = gulpLoadPlugins(); // Last Injection
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+				proxy: "localhost:8888/wjpro/",
+				port: 3000
+	});
+});
 
 gulp.task('sass', function () {
-    gulp.src('./build/scss/main.scss')
-        .pipe(plugins.plumber())
-        .pipe(plugins.sourcemaps.init())
-          .pipe(plugins.sass({
-            includePaths: require('node-bourbon').includePaths,
-            outputStyle: 'compressed'
-          }))
-          .pipe(plugins.autoprefixer({
-              browsers: ['last 2 versions'],
-              cascade: false
-          }))
-				.pipe(plugins.rename('style.css'))
-        .pipe(plugins.sourcemaps.write())
-        .pipe(gulp.dest(paths.stylesheet))
-        .pipe(plugins.notify("Sass Compiled..."))
-        .pipe(plugins.livereload({quiet:true}));
+	gulp.src(paths.build)
+		.pipe(plugins.plumber())
+		.pipe(plugins.sourcemaps.init())
+			.pipe(plugins.sass({
+				includePaths: require('node-bourbon').includePaths,
+				outputStyle: 'compressed'
+			}))
+		.pipe(plugins.rename('style.css'))
+		.pipe(plugins.sourcemaps.write())
+		.pipe(gulp.dest(paths.stylesheet))
+		.pipe(browserSync.stream())
+		.pipe(plugins.notify("Sass Compiled..."));
 });
 
-gulp.task('plugins', function() {
-  gulp.src(['./build/js/*.js'])
-    .pipe(plugins.plumber())
-    .pipe(plugins.concat('min1.js'))
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest(paths.build))
-    .pipe(plugins.notify("Min1 Created.."))
-    .pipe(plugins.livereload())
+gulp.task('final', function () {
+	gulp.src(paths.stylesheet + 'build/style.scss')
+		.pipe(plugins.plumber())
+			.pipe(plugins.sass({
+				includePaths: require('node-bourbon').includePaths,
+				outputStyle: 'compressed'
+			}))
+		.pipe(plugins.rename('layout.css'))
+		.pipe(gulp.dest(paths.stylesheet))
+		.pipe(browserSync.stream())
 });
-
-gulp.task('scripts', function() {
-  gulp.src(['./build/min/min1.js', './build/js/script/script.js'])
-    .pipe(plugins.plumber())
-    .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.concat('min.js'))
-      .pipe(plugins.uglify())
-      .pipe(gulp.dest(paths.js))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(plugins.notify("Min Compiled!"))
-    .pipe(plugins.livereload())
-});
-
 
 gulp.task('watch', function() {
-  plugins.livereload.listen();
-  gulp.watch('./build/**/*', ['sass', 'scripts']);
+  gulp.watch('**/*.scss', ['sass']);
+  gulp.watch("**/*.html").on('change', browserSync.reload);
 });
 
-// Default task
-gulp.task('default', ['sass', 'scripts', 'watch'], function() {
+// Default Task
+gulp.task('default', ['sass', 'browser-sync', 'watch'], function() {
   //other stuff
 });
